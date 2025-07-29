c01f2bcee268b4279adffa2a630fd13cc930bb02
Jim Klassen <klassen.js@gmail.com>
2025-07-29 16:30:21 -0500

    client-example: Make vite proxy work when VITE_BASE_PATH != '/'

--- client-example/vite.config.ts
+++ client-example/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({mode}) => {
     plugins: [react()],
     server: {
       proxy: {
-        '/v1/': {
+        [basePath+'v1/']: {
           target: 'http://localhost:3000/',
           changeOrigin: true,
         },
